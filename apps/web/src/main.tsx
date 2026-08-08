33067874b0951b8ba14e61a220297354d9cd46a0
Vince Liem <vincepaul.liem@gmail.com>
2026-08-08 11:24:00 +0200

    with ALL shadcn ui components

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,11 +4,14 @@ import { createRoot } from "react-dom/client"
 import "@workspace/ui/globals.css"
 import { App } from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { TooltipProvider } from "@workspace/ui/components/tooltip"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </ThemeProvider>
   </StrictMode>
 )
